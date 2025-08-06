a0125a1b7dd9dd3556eee21aef7586e96fce2b5c
dkmmm <121457786+DarkmoonOnDiscord@users.noreply.github.com>
2025-08-05 17:40:50 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,36 +1,4 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFB9BTgqNAE+1EbsQ5+n//NakqkwJc9ILNabW27OLbcEG7QiyStt/higPp4ZcXSQNKi35PkIVhC4K5y3LPsST0w==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFB9BTgqNAE+1EbsQ5+n//Namt1kScPA+b8HewrHu
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+
 @echo off
 title PixelCraft Launcher
 color 0A
@@ -170,4 +138,5 @@ exit /b 1
 :success
 echo.
 echo [+] Script completed!
+
 pause
